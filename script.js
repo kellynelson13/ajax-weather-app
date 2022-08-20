d3844f8b11571961e49f1d2095b27eaccbb2f7f4
Kelly Nelson <kelnel86@gmail.com>
2022-08-19 19:21:55 -0500

    Caches html elements and starts API function

--- script.js
+++ script.js
@@ -5,12 +5,23 @@
 /////Variables
 
 /////Cached Elements
+const $city = $('#city')
+const $temp = $('#temp')
+const $feels = $('#feels')
+const $weather = $('#weather')
+const $input = $('#input')
 
 ////Event Listeners
 
 ////Functions
 function displayInfo (){
     $.ajax('https://api.openweathermap.org/data/2.5/weather?q=portland&appid=9b9f5972625f3491182c4d87b8132964')
+    .then(function(data){
+        console.log(data)
+        $city.text(data.name)
+        $tem
+    })
+
 }
 
 
@@ -22,4 +33,5 @@ function displayInfo (){
 
 
 
-console.log('https://api.openweathermap.org/data/2.5/weather?q=portland&appid=9b9f5972625f3491182c4d87b8132964')
+
+//console.log('https://api.openweathermap.org/data/2.5/weather?q=portland&appid=9b9f5972625f3491182c4d87b8132964')
