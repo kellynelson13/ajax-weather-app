f34da4526830d1f238c8b000ccbc76ed3d9ed361
Kelly Nelson <kelnel86@gmail.com>
2022-08-19 19:03:33 -0500

    Finish HTML

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// api key: 9b9f5972625f3491182c4d87b8132964
+//API link: https://api.openweathermap.org/data/2.5/weather?q=portland&appid=9b9f5972625f3491182c4d87b8132964
+
+/////Variables
+
+/////Cached Elements
+
+////Event Listeners
+
+////Functions
+function displayInfo (){
+    $.ajax('https://api.openweathermap.org/data/2.5/weather?q=portland&appid=9b9f5972625f3491182c4d87b8132964')
+}
+
+
+
+
+
+
+
+
+
+
+console.log('https://api.openweathermap.org/data/2.5/weather?q=portland&appid=9b9f5972625f3491182c4d87b8132964')
