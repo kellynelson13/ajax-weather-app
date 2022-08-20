9d4ed22aebd2e6b1c2e2f0ec77561078cec8f168
Kelly Nelson <kelnel86@gmail.com>
2022-08-19 19:54:22 -0500

    Finishes app functionality

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ const $temp = $('#temp')
 const $feels = $('#feels')
 const $weather = $('#weather')
 const $input = $('#input')
+const $form = $('form')
 
 ////Event Listeners
-
+$form.on("submit", displayInfo)
 ////Functions
-function displayInfo (){
-    $.ajax('https://api.openweathermap.org/data/2.5/weather?q=portland&appid=9b9f5972625f3491182c4d87b8132964')
+function displayInfo (e){
+    e.preventDefault()
+    let userInput = $input.val()
+    if(userInput === '') return
+    $.ajax(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=9b9f5972625f3491182c4d87b8132964`)
     .then(function(data){
         console.log(data)
         $city.text(data.name)
@@ -23,6 +27,8 @@ function displayInfo (){
         $feels.text(Math.floor((data.main.feels_like - 273.15)* 9/5 + 32))
         $weather.text(data.weather[0].description)
         $input.val('')
+    }, function(error){
+        console.log("Something went wrong")
     })
 
 }
